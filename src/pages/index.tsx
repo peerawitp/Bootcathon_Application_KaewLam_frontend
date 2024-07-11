86ec111bbd96678b91e4975e458789a5511fffaa
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-11 23:38:31 +0700

    feat: add tailwindcss, shadcn/ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import "../App.css";
-
+import { Button } from "@/components/ui/button";
 import { useLine } from "../hooks/useLine";
 import { useLineInfo } from "../hooks/useLineInfo";
 
 function IndexPage() {
-  const { liffObject, status, login, logout } = useLine();
+  const { liffObject, status, login } = useLine();
 
   const { idToken, profile, version } = useLineInfo({
     liff: liffObject,
@@ -20,14 +19,12 @@ function IndexPage() {
       <>
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
-            <div>
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                {"Sign in to your account"}
+            <div className="text-center">
+              <h2 className="mt-6 mb-6 text-center text-3xl font-extrabold text-gray-900">
+                {"Sign in to your LINE account"}
               </h2>
 
-              <button onClick={login} type="button">
-                Login
-              </button>
+              <Button onClick={login}>Login</Button>
             </div>
           </div>
         </div>
