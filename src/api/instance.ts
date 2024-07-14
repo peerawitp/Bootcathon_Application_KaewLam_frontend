d474ee674896b1c630c7cb2b859a74d34c925d11
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-14 16:30:34 +0700

    fix: update axios timeout and use useEffect for api calling

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
+import { useLineStore } from "@/stores/lineStore";
 
 const apiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 1000,
+  timeout: 10000,
 });
+
+apiInstance.interceptors.request.use(
+  (config) => {
+    const { idToken } = useLineStore.getState();
+    if (idToken) {
+      config.headers["Authorization"] = `Bearer ${idToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default apiInstance;
--- src/pages/customer/book.tsx
+++ src/pages/customer/book.tsx
@@ -3,24 +3,30 @@ import CustomerLayout from "@/components/layouts/CustomerLayout";
 
 import { useLine } from "@/hooks/useLine";
 import { useLineInfo } from "@/hooks/useLineInfo";
+import { useState, useEffect } from "react";
 
 function CustomerBookPage() {
+  const [message, setMessage] = useState("Fetching...");
   const { liffObject, status } = useLine();
   const { idToken } = useLineInfo({ liff: liffObject, status });
 
   // API Example
-  apiInstance({
-    method: "GET",
-    url: "/",
-  }).then((res) => {
-    console.log(res);
-  });
+  useEffect(() => {
+    apiInstance({
+      method: "GET",
+      url: "/",
+    }).then((res) => {
+      setMessage(res.data);
+    });
+  }, []);
 
   return (
     <CustomerLayout>
       <div>
         <h1>Customer Book Page</h1>
         <p>{idToken}</p>
+
+        <div>Message from API: {message}</div>
       </div>
     </CustomerLayout>
   );
